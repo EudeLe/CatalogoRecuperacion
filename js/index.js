cb70a10191925d299c7c5fbc046af0629ac3f23f
EudeLe <78832863+EudeLe@users.noreply.github.com>
2021-02-10 22:15:56 -0600

    Update index.js

--- js/index.js
+++ js/index.js
@@ -48,7 +48,7 @@ window.addEventListener("load", function() {
             PRODUCTO.datos = datos;
             console.log(JSON.stringify(PRODUCTO));
             localStorage.setItem('MisVentas', JSON.stringify((PRODUCTO)));
-            alert("Compra realizada con exito, para seguir comprando regrese a la lista de productos :)");
+            alert("Ha elegido los productos a comprar, puede ir a confirmar en el ícono del carrito en la opción "+"carrito :)");
         }
     });
 
